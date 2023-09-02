ca57bea5f7b00e94fdd8dafd30b468199b7ef38d
Bob Kraemer <bob.kraemer.git@gmail.com>
2023-09-01 20:28:47 -0400

    Refactoring

--- src/App.java
+++ src/App.java
@@ -15,16 +15,11 @@ public class App {
 
         do {
             print.Messages.report(numTurns);
-            //            String mostToLeastFrequentLetters = Messages.report(numTurns);  //  PRINT The Matrix.  Return the letter counts from the database sorted from most to least frequent in a SET.
-//            Messages.printGeneralStrategies();
-//            words = reportAnalysis(mostToLeastFrequentLetters);  //  ASSESS the Report, suggest strategies, take action (Matrix.size == 0. Matrix.size == 1 and so on).
             print.Messages.strategies(numTurns);
-
-
             print.Messages.specificStrategies();
 
-
             guess = read.Keyboard.guess();                        //  TYPE a guess
+
             response = read.Keyboard.response();                  //  TYPE the response
             numTurns++;                                          //  INCREMENT the counter for the number of turns taken.
 //            insertTurn(guess, response);                         //  Take a turn, and INSERT it into the Matrix.
--- src/analyze/Report.java
+++ src/analyze/Report.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class Report {
     public static void previousGuesses() throws SQLException {
         if (dataStructures.Matrix.truthTable[5][0] == 0) {
-            System.out.println("There are no previous guesses.  I suggest making a guess using the 5 MOST COMMON letters...");
+            System.out.println("There are no previous guesses.  I suggest making a guess using the 5 MOST COMMON letters:");
             ResultSet resultSet = transactSQL.Query.select(morph.LetterCountsString.queryForWords());
             System.out.println(morph.ResultSetTo.strings(resultSet));
         }
