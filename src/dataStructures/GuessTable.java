d8654c48664658263a728c1b94ef401cf1cfaea2
Bob Kraemer <bob.kraemer.git@gmail.com>
2023-09-04 12:11:15 -0400

    Established basic functionality of dataStructures.GuessTable.getNextMostCommon

--- src/dataStructures/GuessTable.java
+++ src/dataStructures/GuessTable.java
@@ -12,8 +12,6 @@ public class GuessTable {
     public static ArrayList<String> threeMostCommon = new ArrayList<>();
     public static ArrayList<String> nextMostCommon = new ArrayList<>();  //  recursive 2thru6~2thru5...26
     boolean previousGuessMade;
-
-
     public static void push() throws SQLException {
 
         ResultSet resultSet = transactSQL.Query.select("select * from Words_tbl where word like '%" +
@@ -56,14 +54,37 @@ public class GuessTable {
         while(resultSet.next()) {
             threeMostCommon.add(resultSet.getString(1));
         }
-
 //DEBUG:
 //        System.out.println("Three Most Common:");
 //        for(String word:threeMostCommon) {
 //            System.out.println(word);
 //        }
+
+        getNextMostCommon(5);
     }
     public static void pop() {
 
+    }
+    public static void getNextMostCommon(int c) throws SQLException {
+
+        ResultSet resultSet = transactSQL.Query.select("select * from Words_tbl where word like '%" +
+                Messages.mostCommonLetters.charAt(1) + "%' and word like '%" +
+                Messages.mostCommonLetters.charAt(2) + "%' and word like '%" +
+                Messages.mostCommonLetters.charAt(3) + "%' and word like '%" +
+                Messages.mostCommonLetters.charAt(4) + "%' and word like '%" +
+                Messages.mostCommonLetters.charAt(c) + "%'");
+
+        if(resultSet.isBeforeFirst()) {
+            while(resultSet.next()) {
+                nextMostCommon.add(resultSet.getString(1));
+            }
+        } else {
+                getNextMostCommon(c+1);
+        }
+//DEBUG:
+//        System.out.println("nextMostCommon:");
+//        for(String word:nextMostCommon) {
+//            System.out.println(word);
+//        }
     }
 }
