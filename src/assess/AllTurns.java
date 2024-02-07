4868a4ade12f35b55a944e3e973ddc0b35604cb0
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-02-07 10:31:33 -0500

    (WIP) assess.AllTurns.makeDeterminations() now contains framework to begin processing single letter changes 'mostly' correctly

--- src/assess/AllTurns.java
+++ src/assess/AllTurns.java
@@ -2,6 +2,7 @@ package assess;
 
 import dataStructures.LetterGroup;
 import dataStructures.Turn;
+import dataStructures.Unknown;
 
 import java.util.LinkedList;
 import java.util.Map;
@@ -23,8 +24,10 @@ public class AllTurns {
         System.out.println("kT: " + knownTogether.letters);
     }
 
-    public static void makeDeterminations(LinkedList<Turn> Turns) {
+    public static void makeDeterminations(LinkedList<Turn> Turns, LetterGroup knownTogether, LetterGroup knownIn, LetterGroup knownOut, Unknown unknown) {
 
+        LetterGroup letterChangedFrom = new LetterGroup();
+        LetterGroup letterChangedTo = new LetterGroup();
         boolean determinationMade = false;
         int combination = 1;
 
@@ -44,7 +47,68 @@ public class AllTurns {
                 }
                 System.out.println("] = " + Turns.get(j).updatedResponse);
                 combination++;
+
+
+
+
+
+
+
+                if(Turns.get(i).updatedResponse == Turns.get(j).updatedResponse) {
+                    forKnownTogether(Turns, knownTogether);
+                } else if (Turns.get(i).updatedResponse - Turns.get(j).updatedResponse == 1) {
+
+                    letterChangedFrom.letters.putAll(Turns.get(i).turn);
+                    letterChangedTo.letters.putAll(Turns.get(j).turn);
+
+                    Set<Character> turn1Keys = Turns.get(j).turn.keySet();
+                    Set<Character> turn2Keys = Turns.get(i).turn.keySet();
+
+                    for(Character c : turn1Keys) {
+                        if(letterChangedFrom.letters.containsKey(c)) letterChangedFrom.letters.remove(c);
+                    }
+
+                    for(Character c : turn2Keys) {
+                        if(letterChangedTo.letters.containsKey(c)) letterChangedTo.letters.remove(c);
+                    }
+                    System.out.println(letterChangedTo.letters + " was changed to MEOW" + letterChangedFrom.letters + " in these two turns");
+
+                    if(letterChangedTo.letters.size()==1 && letterChangedFrom.letters.size()==1) {
+                        System.out.println("With 1 letter changed, and the responses varying by 1, " + letterChangedFrom.letters + " is KNOWN-IN, and " + letterChangedTo.letters + " is KNOWN OUT.  Take appropriate action.");
+                        determinationMade = true;
+                        knownIn.letters.putAll(letterChangedFrom.letters);
+                        knownOut.letters.putAll(letterChangedTo.letters);
+                        //  ToDo: ALL letters in knownTogether may be moved to knownOut
+                        //  ToDo: ALL letters in knownIn, KnownOut to be removed from unknown SOMEHOW
+                        //  ToDo: Update ALL turns with changes (above)
+
+                    } else {
+                        System.out.println("More than 1 letter changed between these 2 turns.  No conclusions may be drawn.");
+                    }
+
+
+
+
+
+                } else if (Turns.get(i).updatedResponse - Turns.get(j).updatedResponse == -1) {
+
+                    letterChangedFrom.letters.putAll(Turns.get(j).turn);
+                    letterChangedTo.letters.putAll(Turns.get(i).turn);
+
+                    Set<Character> turn1Keys = Turns.get(j).turn.keySet();
+                    Set<Character> turn2Keys = Turns.get(i).turn.keySet();
+
+                    for(Character c : turn2Keys) {
+                        if(letterChangedFrom.letters.containsKey(c)) letterChangedFrom.letters.remove(c);
+                    }
+
+                    for(Character c : turn1Keys) {
+                        if(letterChangedTo.letters.containsKey(c)) letterChangedTo.letters.remove(c);
+                    }
+                    System.out.println(letterChangedTo.letters + " was changed to BEEP" + letterChangedFrom.letters + " in these two turns");
+                }
             }
         }
+        System.out.println();
     }
 }
--- src/print/Messages.java
+++ src/print/Messages.java
@@ -39,7 +39,7 @@ public class Messages {
     public static void report(LetterGroup knownIn, LetterGroup knownOut, LetterGroup knownTogether, Unknown unknown, LinkedList<Turn> Turns ) {
         System.out.println("*****************************************************************  REPORT # " + reportNumber + " *****************************************************************************************");
 
-        if(Turns.size() >= 2) assess.AllTurns.makeDeterminations(Turns);
+        if(Turns.size() >= 2) assess.AllTurns.makeDeterminations(Turns, knownTogether, knownIn, knownOut, unknown);
 
         //  PRINT the LinkedHashMaps...
         System.out.println("Known IN: " + knownIn.letters);
